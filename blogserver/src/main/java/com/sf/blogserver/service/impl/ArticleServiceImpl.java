c5e1dc7966e35e31711aff44d67b3fa9b3a471ac
Hh31 <1315695870@qq.com>
2019-10-25 16:19:07 +0800

    提交测试

---
+++ blogserver/src/main/java/com/sf/blogserver/service/impl/ArticleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.sf.blogserver.service.impl;
+
+import com.sf.blogserver.bean.Article;
+import com.sf.blogserver.service.ArticleService;
+
+import java.util.List;
+
+/**
+ * @Discription
+ * @auther Hh
+ * @package com.sf.blogserver.service.impl
+ * @create 2019/10/25 16:14
+ * @Version: 1.0
+ */
+public class ArticleServiceImpl implements ArticleService {
+    @Override
+    public List<Article> getArticleById(Integer articleId) {
+        return null;
+    }
+}
