65a6c062491087690c09ca6c3e1e08bfffef8bdb
Hh31 <1315695870@qq.com>
2019-10-26 21:55:06 +0800

    IssueService

---
+++ blogserver/src/main/java/com/sf/blogserver/service/impl/IssueServiceImlp.java
@@ -0,0 +1,69 @@
+package com.sf.blogserver.service.impl;
+
+import com.sf.blogserver.bean.Issue;
+import com.sf.blogserver.mapper.IssueMapper;
+import com.sf.blogserver.mapper.UserMapper;
+import com.sf.blogserver.service.IssueService;
+import com.sf.blogserver.vo.IssueVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Discription
+ * @auther Hh
+ * @package com.sf.blogserver.service.impl
+ * @create 2019/10/26 14:07
+ * @Version: 1.0
+ */
+@Service
+public class IssueServiceImlp implements IssueService {
+
+    @Autowired
+    IssueMapper issueMapper;
+
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
+    public List<IssueVo> selectAllIssue() {
+        List<IssueVo> issueVos=new ArrayList<>();
+        for (Issue issue :issueMapper.selectAll()) {
+            //注入已有属性
+            IssueVo issueVo=new IssueVo();
+            issueVo.setIssueId(issue.getIssueId());
+            issueVo.setIssueTitle(issue.getIssueTitle());
+            issueVo.setUserId(issue.getUserId());
+            issueVo.setPublishdate(issue.getPublishdate());
+            issueVo.setIssueAnswers(issue.getIssueAnswers());
+            //获取用户昵称
+            issueVo.setUserNickname(userMapper.selectByPrimaryKey(issue.getUserId()).getUserNickname());
+
+            issueVos.add(issueVo);
+        }
+        return issueVos;
+    }
+
+    @Override
+    public IssueVo selectIssueByPrimaryKey(Integer issueId) {
+
+        return null;
+    }
+
+    @Override
+    public int addNewIssue(Issue issue) {
+        return 0;
+    }
+
+    @Override
+    public int updateIssue(Issue issue) {
+        return 0;
+    }
+
+    @Override
+    public int deleteIssue(Issue issue) {
+        return 0;
+    }
+}
