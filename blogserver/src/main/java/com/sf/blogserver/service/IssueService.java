1ea38da365ef70c5dc0a9c2e0bf675498e2330ee
Hh31 <1315695870@qq.com>
2019-10-26 21:54:01 +0800

    IssueService

--- blogserver/src/main/java/com/sf/blogserver/service/IssueService.java
+++ blogserver/src/main/java/com/sf/blogserver/service/IssueService.java
@@ -1,4 +1,18 @@
 package com.sf.blogserver.service;
 
+import com.sf.blogserver.bean.Issue;
+import com.sf.blogserver.vo.IssueVo;
+
+import java.util.List;
+
 public interface IssueService {
+    public List<IssueVo> selectAllIssue();
+
+    public IssueVo selectIssueByPrimaryKey(Integer issueId);
+
+    public int addNewIssue(Issue issue);
+
+    public int updateIssue(Issue issue);
+
+    public int deleteIssue(Issue issue);
 }
